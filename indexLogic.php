16ce10d9c9bd40323372254df61de96ef9050311
z14165899 <30793597+z14165899@users.noreply.github.com>
2017-09-27 19:28:03 -0700

    Add files via upload

---
+++ indexLogic.php
@@ -0,0 +1,25 @@
+<?php require('helpers.php');
+      require('P2_form.php');
+
+    $P2_form=new P2_form($_POST);
+
+    if($P2_form->isSubmitted()) {
+        $firstTerm = $P2_form->get('firstTerm');
+        $factor = $P2_form->get('factor');
+        $terms = $P2_form->get('terms');
+        $type = $P2_form->get('type');
+        $roundup = $P2_form->get('roundup');
+        $result = $P2_form->get('result');
+        if($roundup==false){
+            $results=$P2_form->answer($firstTerm,$factor,$terms,$type,$result);
+        }else{
+            $results=round($P2_form->answer($firstTerm,$factor,$terms,$type,$result));
+        }
+    }else{
+        $firstTerm=null;
+        $factor =null;
+        $terms=null;
+        $type = null;
+        $roundup = false;
+        $result = 'choose';
+    }
